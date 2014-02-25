d61a9388a1ec0e90949a9393ec8e35923e13b885
Oleiade <tcrevon@gmail.com>
2014-02-25 05:56:48 -0800

    Fix setup.py license mentions

--- setup.py
+++ setup.py
@@ -47,16 +47,15 @@ setup(
     install_requires=[
         'PyYaml',
     ],
-    license="BSD",
+    license="MIT",
     zip_safe=False,
     keywords='etcaetera',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
