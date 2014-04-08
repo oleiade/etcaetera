11a07f3008666ca3b1b965129310f6dd2cf1e66c
Oleiade <tcrevon@gmail.com>
2014-04-08 10:10:07 +0200

    Add is_nested_key util

--- etcaetera/utils.py
+++ etcaetera/utils.py
@@ -1,2 +1,14 @@
+from etcaetera.exceptions import MalformationError
+
+
 def format_key(key):
     return key.strip().upper().replace(' ', '_')
+
+
+def is_nested_key(key):
+    if (key.startswith('.') or
+        key.endswith('.') or
+        '..' in key):
+        raise MalformationError 
+
+    return '.' in key
