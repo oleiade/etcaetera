9b1b4939e82dfc428e507f2ef5ae22973a7afc0c
Oleiade <tcrevon@gmail.com>
2014-04-08 11:03:08 +0200

    Support nested key getitem on adapters

--- etcaetera/adapter/base.py
+++ etcaetera/adapter/base.py
@@ -1,3 +1,6 @@
+from etcaetera.utils import is_nested_key
+
+
 class Adapter(object):
     def __init__(self, *keys, **mapping):
         self.data = {} 
@@ -7,6 +10,23 @@ class Adapter(object):
 
     def __repr__(self):
         return '<{} {}>'.format(self.__str__(), id(self))
+
+    def __getitem__(self, key):
+        if is_nested_key(key):
+            subkeys = key.split('.')
+            return reduce(lambda d, k: d[k], subkeys, self.data)
+
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        if nested_key(key):
+            subkeys = key.split('.')
+            subdict = self.data
+
+            for key in subkeys:
+                subdict = subdict[key]
+
+            
 
     def load(self):
         raise NotImplementedError
--- tests/adapter/test_base.py
+++ tests/adapter/test_base.py
@@ -1,9 +1,31 @@
 import pytest
 
 from etcaetera.adapter import Adapter
+from etcaetera.exceptions import MalformationError
 
 
 class TestAdapter:
+    def test___getitem___with_existing_nested_key_returns_its_value(self):
+        adapter = Adapter()
+        adapter.data['abc'] = {}
+        adapter.data['abc']['123'] = 'easy as'
+
+        assert adapter['abc.123'] == adapter['abc']['123']
+
+    def test___getitem___with_flat_existing_key_returns_its_value(self):
+        adapter = Adapter()
+        adapter.data['abc'] = 'easy as'
+
+        assert adapter['abc'] == 'easy as'
+
+    def test___getitem___with_invalid_nested_key_raises(self):
+        adapter = Adapter()
+        adapter.data['abc'] = {}
+        adapter.data['abc']['123'] = 'easy as'
+
+        with pytest.raises(MalformationError):
+            adapter['..abc']
+        
     def test_load_is_not_implemented(self):
         adapter = Adapter()
 
