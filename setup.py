3ed52af19349edb69cf7e3fa9bc3b1017974cfda
Oleiade <tcrevon@gmail.com>
2014-04-23 17:33:08 +0200

    Version bump 0.4.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='etcaetera',
-    version='0.4.1',
+    version='0.4.2',
     description='Manage multiple configuration sources in a single place',
     long_description=readme + '\n\n' + history,
     author='Oleiade',
